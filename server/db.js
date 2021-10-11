e8d4e48869efa3cbe72b287e1f997fad2730b3d2
Michelle Berger <bergerm613@gmail.com>
2021-10-11 15:50:29 -0400

    stop showing everyone your password

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
-const Pool = require('pg').Pool;
+const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'ilovecandy',
-    post: '5432',
-    database: 'perntodo'
+  user: "postgres",
+  password: "***",
+  post: "5432",
+  database: "perntodo",
 });
 
 module.exports = pool;
